da28678b5ff8a6015ce07175ab156e7060ccb13b
ellsphillips <elliott.phillips.dev@gmail.com>
2022-11-14 13:56:33 +0000

    Enhance: gradient constructor

--- govdash/style/colour/core.py
+++ govdash/style/colour/core.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from typing import cast
+from dataclasses import dataclass
+from typing import Iterable, Optional, cast
+
+import colorir
 
 RGBValue = tuple[int, int, int]
 
@@ -13,3 +16,18 @@ def hex_to_rgb(hex_value: str) -> RGBValue:
 
 def rgb_to_hex(rgb_value: RGBValue) -> str:
     return "#" + "".join([f"{v:x}".ljust(2, "0") for v in rgb_value])
+
+
+@dataclass
+class Gradient:
+
+    colours: Iterable[str]
+    steps: Optional[int] = None
+
+    def __post_init__(self) -> None:
+        grad = colorir.Grad(colors=self.colours)
+        self.__values: list[str] = grad.n_colors(self.steps)
+
+    @property
+    def values(self) -> list[str]:
+        return self.__values
